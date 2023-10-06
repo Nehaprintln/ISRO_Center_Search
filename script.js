70d7f1240c5f2f27091350740039f6320924eb77
Neha <ray.93gupta@gmail.com>
2023-10-06 11:20:50 +0530

    Initial stage - to create ISRO Search Center project using html, css and js.

---
+++ script.js
@@ -0,0 +1,32 @@
+const searchInput = document.querySelector('.search_input');
+const searchIcon = document.querySelector('.search_icon');
+const displayDetails = document.querySelector('.display_details');
+
+const radioBtns = document.querySelectorAll('input[name="location"]');
+// ==== select radio value  ====
+var selectedLocation;
+let findSelecting = ()=>{
+    selectedLocation = document.querySelector('input[name="location"]:checked').value;
+    console.log(selectedLocation);
+};
+
+radioBtns.forEach(radioBtn => {
+    radioBtn.addEventListener('change', findSelecting)
+});
+
+//  ===  get input text ===
+var input;
+searchIcon.addEventListener('click', ()=>{
+    input = searchInput.value.trim();
+    console.log(input);
+    searchInput.value = '';
+});
+
+// ===  get data from link ===
+const filterData = data.filter(data => {
+    return data[selectedLocation].includes(input);
+});
+
+filterData.forEach(displayData => {
+    
+})
